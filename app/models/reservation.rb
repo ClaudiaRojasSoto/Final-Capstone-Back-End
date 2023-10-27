78274edaa509163b47b4d3f322a2543d7fc7793b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-27 00:49:11 -0300

    Add the reservation model with validations and the reservation with car method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates :start_time, :end_time, :city, presence: true
+
+  def reservation_with_car
+    {
+      reservation: self.attributes,
+      car: self.car.attributes
+    }
+  end
 end
