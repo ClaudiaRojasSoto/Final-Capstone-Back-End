181f421ee158661cbd667a51a6ec0d23cdb75844
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-27 12:00:21 -0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :reservations 
+  has_many :reservations
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
