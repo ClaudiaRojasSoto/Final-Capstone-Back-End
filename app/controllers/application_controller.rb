a99329e9625592418ced8a6420477d05bfe2811d
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-27 12:12:17 -0300

    Delete comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::API
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  # helper_method :current_user
 
   private
 
