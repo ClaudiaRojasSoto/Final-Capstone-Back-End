888894dea48bc540ba2015cff6f260d372843a06
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-30 22:56:57 -0300

    Creates on user controller the index and show method to handle the users more easily

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,14 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
