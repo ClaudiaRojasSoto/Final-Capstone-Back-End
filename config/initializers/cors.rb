73e4e2df7d335dbdc3e7495d499823925d2534ca
PabloBona <pablobonasera@gmail.com>
2023-11-08 21:24:14 -0300

    render

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://driver-front.onrender.com/'
+    origins 'https://driver-front.onrender.com/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
